041ac66884fb7d75c59135affc140c53ea6a1a09
Tevin <brwntvn@yahoo.com>
2023-10-02 22:54:24 -0500

    added two new files

---
+++ README.md
@@ -0,0 +1,1 @@
+A personal website to display front end developer skills and progress.
---
+++ javascript.js
@@ -0,0 +1,4 @@
+const myForm = document.getElementById("form")
+
+myForm.addEventListener("Submit", function(){
+    document.getElementsByName(success.html)})
